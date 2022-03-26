83449fb5f50fef59ce4d1040899ea9bbd58e3bd8
Rafael Olivay <rafaelsebastian@live.com.ar>
2022-03-26 14:36:30 -0300

    end of section 5 authentication and authorization added

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -7,16 +7,17 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
-import { TaskStatus } from './task-status.enum';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task.dto';
 import { Task } from './task.entity';
-import { DeleteResult } from 'typeorm';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard()) //protect entire controller
 export class TaskController {
   constructor(private service: TaskService) {}
 
